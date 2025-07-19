89fa81e30ad0a8d3eb3dde84ea77284ade05ccdf
or orsatti <antonio@10play.dev>
2025-07-19 20:51:53 +0300

    feat: add api for parse tags

--- parse_tags/parse_tags.go
+++ parse_tags/parse_tags.go
@@ -2,7 +2,6 @@ package parsetags
 
 import (
 	"errors"
-	"fmt"
 	"reflect"
 )
 
@@ -16,16 +15,22 @@ const (
 
 type TagParser struct {
 	original   any
-	ref        reflect.Type
 	idField    string
 	textFields []string
+	refVal     reflect.Value
+	invalid    bool
 }
 
 func NewTagParser(o any) *TagParser {
 	r := reflect.TypeOf(o)
 	tp := &TagParser{
 		original: o,
-		ref:      r,
+		refVal:   reflect.ValueOf(o),
+	}
+
+	if r.Kind() != reflect.Struct {
+		tp.invalid = true
+		return tp
 	}
 
 	for i := range r.NumField() {
@@ -48,11 +53,34 @@ func NewTagParser(o any) *TagParser {
 }
 
 func (t *TagParser) GetID() (string, error) {
-	val, ok := t.ref.FieldByName(t.idField)
-	if !ok {
-		return "", errors.New("Missing ID field")
+	if t.invalid {
+		return "", errors.New("acting on invalid tag parser")
 	}
 
-	fmt.Println(val)
-	return "", nil
+	idVal := t.refVal.FieldByName(t.idField)
+	if idVal.Kind() != reflect.String {
+		return "", errors.New("id must be string")
+	}
+
+	return idVal.String(), nil
+}
+
+func (t *TagParser) GetText() ([]string, error) {
+	if t.invalid {
+		return nil, errors.New("acting on invalid tag parser")
+	}
+
+	var vals []string
+
+	for _, field := range t.textFields {
+		txtVal := t.refVal.FieldByName(field)
+		if txtVal.Kind() != reflect.String {
+			return nil, errors.New("all text fields must be string")
+		}
+
+		vals = append(vals, txtVal.String())
+
+	}
+
+	return vals, nil
 }
--- parse_tags/parse_tags_test.go
+++ parse_tags/parse_tags_test.go
@@ -1,20 +1,93 @@
 package parsetags
 
-import "testing"
+import (
+	"testing"
 
-type testStruct struct {
-	id          string
-	name        string
-	description string
+	"github.com/stretchr/testify/assert"
+)
+
+type User struct {
+	ID          string `index:"id"`
+	Name        string `index:"text"`
+	Description string
 }
 
 func TestGetID(t *testing.T) {
-	test := testStruct{
-		id:          "123",
-		name:        "Jane",
-		description: "A masterious woman",
+	testCases := []struct {
+		name string
+		u    User
+		id   string
+	}{
+		{
+			name: "empty",
+			u:    User{},
+			id:   "",
+		},
+		{
+			name: "with id",
+			u: User{
+				ID:          "123",
+				Name:        "Jane",
+				Description: "A masterious woman",
+			},
+			id: "123",
+		},
 	}
 
-	tp := NewTagParser(test)
-	tp.GetID()
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(tt *testing.T) {
+			tp := NewTagParser(testCase.u)
+
+			id, err := tp.GetID()
+			assert.NoError(tt, err)
+			assert.Equal(tt, testCase.id, id)
+		})
+	}
+}
+
+func TestGetID_Err(t *testing.T) {
+	tp := NewTagParser(true)
+
+	_, err := tp.GetID()
+	assert.Error(t, err)
+}
+
+func TestGetText(t *testing.T) {
+	testCases := []struct {
+		u    User
+		text []string
+		name string
+	}{
+		{
+			name: "empty",
+			u:    User{},
+			text: []string{""},
+		},
+		{
+			name: "with text",
+			u: User{
+				ID:          "123",
+				Name:        "Jane",
+				Description: "A masterious woman",
+			},
+			text: []string{"Jane"},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(tt *testing.T) {
+			tp := NewTagParser(testCase.u)
+
+			text, err := tp.GetText()
+			assert.NoError(tt, err)
+			assert.Equal(tt, testCase.text, text)
+		})
+	}
+}
+
+func TestGetText_Err(t *testing.T) {
+	tp := NewTagParser(true)
+
+	_, err := tp.GetText()
+	assert.Error(t, err)
 }
